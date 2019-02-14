a8fce68aea5a8bd54ae455c79747fcb1a98ce689
im-slow <andrea.amicosante@gmail.com>
2019-02-14 13:47:53 +0100

    arhivio funzionante al click

--- resources/views/Contents/archive.blade.php
+++ resources/views/Contents/archive.blade.php
@@ -1,10 +1,10 @@
 @foreach($data as $blog)
 <li class="p-b-7">
-    <a href="#" onclick="window.location='{{ route('data', ["mese" => $blog->mese, "anno" => $blog->anno]) }}'" class="flex-w flex-sb-m stext-115 cl6 hov-cl1 trans-04 p-tb-2">
+    <a href="#" onclick="window.location='{{ route('data', ["mese" => $blog->mese, "anno" => $blog->anno, $blog->num]) }}'" class="flex-w flex-sb-m stext-115 cl6 hov-cl1 trans-04 p-tb-2">
         <span>
             {{ $blog->mese }} {{ $blog->anno }}
         </span>
-            ({{ $blog->num }})
+            {{ $blog->num }}
         <span>
 
         </span>
