cb6ca02b2382c7fc01cdb46e88b0a98a231b614a
ITh0rn <angelo.dalfonso@student.univaq.it>
2019-02-18 19:08:16 +0100

    update

--- resources/views/Contents/tableordini.blade.php
+++ resources/views/Contents/tableordini.blade.php
@@ -1,24 +1,21 @@
+
 <h5 class="mtext-108 cl2 p-b-25">
     Storico ordini:
 </h5>
 
 <table class="table-profile p-t-30 m-b-30">
     <tr class="table_head">
-        <th class="column-2 p-l-50">N° Ordine</th>
+        <th class="column-2 p-l-50">Codice</th>
         <th class="column-2">Prodotti</th>
-        <th class="column-2">Sconto</th>
         <th class="column-3">Totale</th>
-        <th class="column-5 txt-right">Indirizzo</th>
         <th class="column-6 pl-50">vedi di più</th>
     </tr>
     @if(count($ordini) > 0)
     @foreach($ordini as $ordine)
             <tr class="table_row">
-                <td class="column-2 p-l-50">{{$ordine->ID}}</td>
-                <td class="column-2">{{$ordine->prodotti}}</td>
-                <td class="column-2">{{$ordine->sconto}}</td>
+                <td class="column-2 p-l-50">#{{$ordine->ID}}</td>
+                <td class="column-2">visualizza</td>
                 <td class="column-3">{{$ordine->totale}}</td>
-                <td class="column-5"><p>{{$ordine->citta}}, {{$ordine->provincia}} {{$ordine->cap}}</p><p>{{$ordine->via}} {{$ordine->civico}}</p></td>
                 <td class="column-5"><i class="icon-header-item hov-cl1 cl2 zmdi zmdi-assignment-o p-l-30"></i></td>
             </tr>
     @endforeach
